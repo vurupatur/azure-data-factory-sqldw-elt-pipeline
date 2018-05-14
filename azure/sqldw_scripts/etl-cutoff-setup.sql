d8f13372d6b445ae5d82e54c3388a6f2a33af43f
Nithin M <nimadha@microsoft.com>
2018-05-14 16:46:51 -0700

    updating the name of the tables

--- azure/sqldw_scripts/etl-cutoff-setup.sql
+++ azure/sqldw_scripts/etl-cutoff-setup.sql
@@ -1,7 +1,7 @@
-INSERT INTO [Integration].[ETLCutoff] values ('City','1-1-1900')
-INSERT INTO [Integration].[ETLCutoff] values ('Sale','1-1-1900')
-INSERT INTO [Integration].[ETLCutoff] values ('Customer','1-1-1900')
-INSERT INTO [Integration].[ETLCutoff] values ('Employee','1-1-1900')
-INSERT INTO [Integration].[ETLCutoff] values ('Stock Item','1-1-1900')
+INSERT INTO [Integration].[ETLCutoff] values ('[Dimension].[City]','1-1-1900')
+INSERT INTO [Integration].[ETLCutoff] values ('[Fact].[Sale]','1-1-1900')
+INSERT INTO [Integration].[ETLCutoff] values ('[Dimension].[Customer]','1-1-1900')
+INSERT INTO [Integration].[ETLCutoff] values ('[Dimension].[Employee]','1-1-1900')
+INSERT INTO [Integration].[ETLCutoff] values ('[Dimension].[StockItem]','1-1-1900')
 
 
